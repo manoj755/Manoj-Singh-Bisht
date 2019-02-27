78d1787721fa836ac4a5b92c0d4579d7322713f4
passive <passive@passive-Aspire-ES1-572>
2019-02-27 10:26:29 +0530

    Merge origin/master
    Conflicts:
    	js/Ats/calldetail.js

--- src/app/control/add-to-job/add-to-job.component.ts
+++ src/app/control/add-to-job/add-to-job.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DBService } from 'app/db.service';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-to-job',
@@ -19,11 +22,25 @@ export class AddToJobComponent implements OnInit {
   store: any = {};
   emailselected: any = {};
   smsselected: any = {};
+  // filteredOptions = ['first', 'second', 'three'];
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
   constructor(private db: DBService) {
 
   }
 
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
   addtojobcandidates(): void {
 
